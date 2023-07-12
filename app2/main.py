8666350c85e7e327cd3d3b2ff1d9f4da9c7889ba
Wout Besseling <wbesseling13@gmail.com>
2023-07-12 10:20:37 +0200

    Finished initial stage of app 2

--- app2/main.py
+++ app2/main.py
@@ -3,50 +3,87 @@ import os
 import sys
 from string import ascii_uppercase
 import random as rng
-
 import qrcode
+import datetime
 from qrcode.image.pure import PyPNGImage
 
-qr = qrcode.QRCode()
 
 class QRSCANNER():
     
     def __init__(self):
-        data = self.read_USB()
-        letters = self.create_Extra_Code()
-        img = self.create_QR(data+letters)
-        with open("/app2")
+        self.find_all_keys_and_convert()
+        
+    def find_all_keys_and_convert(self):
+        keys = self.read_USB()
+        if not keys:
+            print("no keys found")
+        for key in keys:
+            with open(self.get_USB_root()+key, encoding = "utf-8") as f:
+                data = f.read()
+            letters = self.create_extra_code()
+            img = self.create_QR(data+letters)
+            self.save(img, f"QR_{letters}.png")
+        
+    def save(self, img: PyPNGImage, name: str):
+        path = os.getcwd() + "/app2/dump/"
+        img.save(path+name)
+        
+        # save to usb stick if inserted
+        root = self.get_USB_root(False)
+        if not root:
+            return
+        img.save(root + name)
 
-    def get_USB_root(self) -> str:
+    def get_USB_root(self, check_for_keys=True) -> str:
         """Scans for drives D: through Z:"""
         for drive in ascii_uppercase[:-24:-1]:
-            file_path = f"{drive}:/keys/"
+            file_path = f"{drive}:/"
             if os.path.exists(file_path):
-                return file_path
+                onlyfiles = [f for f in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, f))]
+                has_keys = False
+                for file in onlyfiles:
+                    if "key" in file:
+                        has_keys = True
+                        if check_for_keys:
+                            return file_path
+                if not has_keys:
+                    return file_path
+                
 
         print('error, file not found')
-        sys.exit(1)
+        return None
 
 
-    def read_USB(self, keyno=1) -> str:
+    def read_USB(self) -> list:
         """Returns the contents of the file as a string"""
         root = self.get_USB_root()
-        file_path = root + "key" + str(keyno) + ".txt"
-        with open(file_path) as file:
-            data = file.read()
-        return data
+        onlyfiles = [f for f in os.listdir(root) if os.path.isfile(os.path.join(root, f))]
+        txtfiles = []
+        for file in onlyfiles:
+            if ".txt" in file:
+                txtfiles.append(file)
+        return txtfiles
     
-    def create_QR(data):
-        qr.add_data(data)
-        qr.make()
+    def create_QR(self, data) -> PyPNGImage:
         img = qrcode.make(data, image_factory=PyPNGImage)
         return img
     
-    def create_Extra_Code() -> str:
-        indexes = [0,1,2,3]
+    def create_extra_code(self) -> str:
         letters = ""
-        for i in indexes:
-           letters += ascii_uppercase[rng.randrange(1, 27)]
-        return letters
+        i = 0
+        while i < 4:
+            letters += ascii_uppercase[rng.randrange(1, 26)]
+            i+=1
+        self.save_extra_code(letters)
+        return "_" + letters
+    
+    def save_extra_code(self, letters):
+        root = os.getcwd()
+        try:
+            f = open(root+f"/app2/dump/{letters}.txt", 'x+', encoding="utf-8")
+        except FileExistsError:
+            f = open(root+f"/app2/dump/{letters}.txt", 'r+', encoding="utf-8")
+        f.writelines(letters + " " + datetime.datetime.now(tz=None).strftime("%d/%m/%Y"))
+        f.close()
 
 qr = QRSCANNER()
